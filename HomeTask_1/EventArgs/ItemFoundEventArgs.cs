eb896575d85f2257c923acb607661223f6f3b247
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-21 23:31:01 +0200

    added: action type enum.        event args class for events that represent found item

---
+++ HomeTask_1/EventArgs/ItemFoundEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using HomeTask_1.Enums;
+
+namespace HomeTask_1.EventArgs
+{
+    public class ItemFoundEventArgs<T> : System.EventArgs
+        where T : FileSystemInfo
+    {
+        public T FoundItem { get; set; }
+        public BehaviorAction Action { get; set; }
+    }
+}
