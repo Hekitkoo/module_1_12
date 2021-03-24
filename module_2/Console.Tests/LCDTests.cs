889c6a962d8a0cd4db66502e072e10448fbb303e
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 20:16:06 +0200

    Added LCD.cs and LCDTests.cs for LCD Digits Kata

---
+++ module_2/Console.Tests/LCDTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Console.Tests
+{
+    public class LCDTests
+    {
+        private LCD lcd;
+
+        [SetUp]
+        public void Setup()
+        {
+            lcd = new LCD();
+        }
+
+        [Test]
+        public void ToLCD_WrongInput_StringIsEmpty()
+        {
+            var testInput = "a";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void ToLCD_RightZeroInput_ReturnStringAsExpected()
+        {
+            var testInput = 0.ToString();
+            var expectedResult = $"._.{Environment.NewLine}|.|{Environment.NewLine}|_|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ module_2/Console/LCD.cs
@@ -0,0 +1,10 @@
+﻿namespace Console
+{
+    public class LCD
+    {
+        public string ToLCD(string number)
+        {
+            return string.Empty;
+        }
+    }
+}
