849d5760dc0f927f7426056154cd34916aeeae20
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 17:41:51 +0200

    Added prints "Even instead of number, if the number is even, means divisible by 2" tests

--- module_2/Console.Tests/OddEvenTests.cs
+++ module_2/Console.Tests/OddEvenTests.cs
@@ -9,6 +9,7 @@ namespace Console.Tests
         private const int StartIndex = 1;
         private const int EndIndex = 100;
         private const int TestRangeEndIndex = 2;
+        private const string EvenText = "Even";
         private OddEven oddEven;
 
         [SetUp]
@@ -28,7 +29,7 @@ namespace Console.Tests
         [Test]
         public void Print_RangeAreSet_ResultIsSameAsExpected()
         {
-            var expectedResult = $"{StartIndex}, {TestRangeEndIndex}";
+            var expectedResult = $"{StartIndex}, {EvenText}";
 
             var result = oddEven.Print(StartIndex, TestRangeEndIndex);
 
@@ -41,6 +42,16 @@ namespace Console.Tests
             Action action = () => oddEven.Print(TestRangeEndIndex, StartIndex);
 
             action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Print_RangeAreSetWithEvenRange_EvenTextResult()
+        {
+            var expectedResult = EvenText;
+
+            var result = oddEven.Print(TestRangeEndIndex, TestRangeEndIndex);
+
+            result.Should().Be(expectedResult);
         }
     }
 }
