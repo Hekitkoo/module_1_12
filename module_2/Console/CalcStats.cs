4f65446c072304619a0be9ba0c5dcf3985ddd3ea
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 19:50:15 +0200

    Refactored CalcStats.cs (added generic method for messages)

--- module_2/Console/CalcStats.cs
+++ module_2/Console/CalcStats.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -6,21 +7,53 @@ namespace Console
 {
     public class CalcStats
     {
+        private const string MinValueText = "minimum value";
+        private const string MaxValueText = "maximum value";
+        private const string NumbersLengthText = "number of elements in the sequence";
+        private const string AverageValueText = "average value";
+
         public string Process(int[] numbers)
         {
             Validate(numbers);
 
             var stringBuilder = new StringBuilder();
-            var minimumValue = numbers.Min();
-            var maxValue = numbers.Max();
-            var numbersLength = numbers.Length;
-            var averageValue = numbers.Average();
-            stringBuilder.AppendLine($"o) minimum value = {minimumValue}");
-            stringBuilder.AppendLine($"o) maximum value = {maxValue}");
-            stringBuilder.AppendLine($"o) number of elements in the sequence = {numbersLength}");
-            stringBuilder.AppendLine($"o) average value = {averageValue}");
+
+            var minimumValue = GetMin(numbers);
+            var maxValue = GetMax(numbers);
+            var numbersLengthValue = GetLength(numbers);
+            var averageValue = GetAverage(numbers);
+
+            stringBuilder.AppendLine(GetMessage(minimumValue.ToString(), MinValueText));
+            stringBuilder.AppendLine(GetMessage(maxValue.ToString(), MaxValueText));
+            stringBuilder.AppendLine(GetMessage(numbersLengthValue.ToString(), NumbersLengthText));
+            stringBuilder.AppendLine(GetMessage(averageValue.ToString(), AverageValueText));
 
             return stringBuilder.ToString();
+        }
+
+        private string GetMessage(string value, string minValueText)
+        {
+            return $"o) {minValueText} = {value}";
+        }
+
+        private double GetAverage(int[] numbers)
+        {
+            return numbers.Average();
+        }
+
+        private int GetLength(IReadOnlyCollection<int> numbers)
+        {
+            return numbers.Count;
+        }
+
+        private int GetMax(IEnumerable<int> numbers)
+        {
+            return numbers.Max();
+        }
+
+        private int GetMin(IEnumerable<int> numbers)
+        {
+            return numbers.Min();
         }
 
         private void Validate(int[] numbers)
