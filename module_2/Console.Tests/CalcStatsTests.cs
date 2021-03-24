cae4ddd0361c018e1e868b002f90a2844db3f90d
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 18:31:28 +0200

    Added statistic minimum value test

--- module_2/Console.Tests/CalcStatsTests.cs
+++ module_2/Console.Tests/CalcStatsTests.cs
@@ -6,6 +6,7 @@ namespace Console.Tests
 {
     public class CalcStatsTests
     {
+        private static readonly int[] TestArray = {6, 9, 15, -2, 92, 11};
         private CalcStats calcStats;
 
         [SetUp]
@@ -22,6 +23,16 @@ namespace Console.Tests
             Action action = () => calcStats.Process(emptyArray);
 
             action.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void Process_IntArrayIsCorrect_MinimumValueResultTextContains()
+        {
+            var expectedText = "o) minimum value = -2";
+
+            var result = calcStats.Process(TestArray);
+
+            result.Should().Contain(expectedText);
         }
     }
 }
