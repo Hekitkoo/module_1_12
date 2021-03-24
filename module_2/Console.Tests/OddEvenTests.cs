154ef11a9bbc486e2ee37fb87110404ebc6bb7d2
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 17:28:30 +0200

    Added prints numbers tests.

--- module_2/Console.Tests/OddEvenTests.cs
+++ module_2/Console.Tests/OddEvenTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -7,6 +8,7 @@ namespace Console.Tests
     {
         private const int StartIndex = 1;
         private const int EndIndex = 100;
+        private const int TestRangeEndIndex = 2;
         private OddEven oddEven;
 
         [SetUp]
@@ -21,6 +23,24 @@ namespace Console.Tests
             var result = oddEven.Print(StartIndex, EndIndex);
 
             result.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void Print_RangeAreSet_ResultIsSameAsExpected()
+        {
+            var expectedResult = $"{StartIndex}, {TestRangeEndIndex}";
+
+            var result = oddEven.Print(StartIndex, TestRangeEndIndex);
+
+            result.Should().BeSameAs(expectedResult);
+        }
+
+        [Test]
+        public void Print_RangeAreSetWrong_ThrowException()
+        {
+            Action action = () => oddEven.Print(StartIndex, TestRangeEndIndex);
+
+            action.Should().Throw<ArgumentException>();
         }
     }
 }
