a1c9b310bf83e4c149af1a0f006567f493d9c4af
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 22:21:03 +0200

    Added One-Nine tests

--- module_2/Console.Tests/LCDTests.cs
+++ module_2/Console.Tests/LCDTests.cs
@@ -34,5 +34,104 @@ namespace Console.Tests
 
             result.Should().Be(expectedResult);
         }
+
+        [Test]
+        public void ToLCD_RightOneInput_ReturnStringAsExpected()
+        {
+            var testInput = 1.ToString();
+            var expectedResult = $"...{Environment.NewLine}..|{Environment.NewLine}..|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightTwoInput_ReturnStringAsExpected()
+        {
+            var testInput = 2.ToString();
+            var expectedResult = $"._.{Environment.NewLine}._|{Environment.NewLine}|_.";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightThreeInput_ReturnStringAsExpected()
+        {
+            var testInput = 3.ToString();
+            var expectedResult = $"._.{Environment.NewLine}._|{Environment.NewLine}._|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightFourInput_ReturnStringAsExpected()
+        {
+            var testInput = 4.ToString();
+            var expectedResult = $"...{Environment.NewLine}|_|{Environment.NewLine}..|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightFiveInput_ReturnStringAsExpected()
+        {
+            var testInput = 5.ToString();
+            var expectedResult = $"._.{Environment.NewLine}|_.{Environment.NewLine}._|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightSixInput_ReturnStringAsExpected()
+        {
+            var testInput = 6.ToString();
+            var expectedResult = $"._.{Environment.NewLine}|_.{Environment.NewLine}|_|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightSevenInput_ReturnStringAsExpected()
+        {
+            var testInput = 7.ToString();
+            var expectedResult = $"._.{Environment.NewLine}..|{Environment.NewLine}..|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightEightInput_ReturnStringAsExpected()
+        {
+            var testInput = 8.ToString();
+            var expectedResult = $"._.{Environment.NewLine}|_|{Environment.NewLine}|_|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void ToLCD_RightNineInput_ReturnStringAsExpected()
+        {
+            var testInput = 9.ToString();
+            var expectedResult = $"._.{Environment.NewLine}|_|{Environment.NewLine}..|";
+
+            var result = lcd.ToLCD(testInput);
+
+            result.Should().Be(expectedResult);
+        }
     }
 }
