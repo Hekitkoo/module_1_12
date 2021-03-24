eee4df6af50dad51a39563edb029a03ce91da468
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 18:38:51 +0200

    Added statistic minimum value implementation

--- module_2/Console/CalcStats.cs
+++ module_2/Console/CalcStats.cs
@@ -1,10 +1,28 @@
-﻿namespace Console
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Console
 {
     public class CalcStats
     {
         public string Process(int[] numbers)
         {
-            return string.Empty;
+            Validate(numbers);
+
+            var stringBuilder = new StringBuilder();
+            var minimumValue = numbers.Min();
+            stringBuilder.Append($"o) minimum value = {minimumValue}");
+
+            return stringBuilder.ToString();
+        }
+
+        private void Validate(int[] numbers)
+        {
+            if (numbers.Length == default)
+            {
+                throw new ArgumentException();
+            }
         }
     }
 }
