a30fdaa99c3d59f8bd5c63c592a2e533a4183981
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 22:08:50 +0200

    Added Zero implementation

--- module_2/Console/LCD.cs
+++ module_2/Console/LCD.cs
@@ -1,10 +1,34 @@
-﻿namespace Console
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Console.Models;
+
+namespace Console
 {
     public class LCD
     {
+        private IDictionary<char, Digit> digits = new Dictionary<char, Digit>
+        {
+            {'0', new Digit("._.", "|.|", "|_|")}
+        };
+
         public string ToLCD(string number)
         {
-            return string.Empty;
+            var chars = Pars(number);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var symbol in chars)
+            {
+                digits.TryGetValue(symbol, out var value);
+                stringBuilder.Append(value);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private char[] Pars(string number)
+        {
+            return number.ToCharArray();
         }
     }
 }
