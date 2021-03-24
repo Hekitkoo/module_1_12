09067be5f1d3c002ce7a0b25edddef6cb064ab10
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 18:27:13 +0200

    Added CalcStats.cs and CalcStatsTests.cs files.

---
+++ module_2/Console.Tests/CalcStatsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Console.Tests
+{
+    public class CalcStatsTests
+    {
+        private CalcStats calcStats;
+
+        [SetUp]
+        public void Setup()
+        {
+            calcStats = new CalcStats();
+        }
+
+        [Test]
+        public void Process_IntArrayIsEmpty_ArgumentException()
+        {
+            var emptyArray = Array.Empty<int>();
+
+            Action action = () => calcStats.Process(emptyArray);
+
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ module_2/Console/CalcStats.cs
@@ -0,0 +1,10 @@
+﻿namespace Console
+{
+    public class CalcStats
+    {
+        public string Process(int[] numbers)
+        {
+            return string.Empty;
+        }
+    }
+}
