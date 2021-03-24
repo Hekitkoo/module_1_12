ac69d5291d41dbe77eeed51acf464b1ac31e7730
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-03-24 23:18:25 +0200

    Added One-Nine implementation

--- module_2/Console/LCD.cs
+++ module_2/Console/LCD.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using Console.Models;
 
@@ -7,9 +6,30 @@ namespace Console
 {
     public class LCD
     {
-        private IDictionary<char, Digit> digits = new Dictionary<char, Digit>
+        private static readonly IDictionary<int, string> Bits = new Dictionary<int, string>
         {
-            {'0', new Digit("._.", "|.|", "|_|")}
+            {0, "._."},
+            {1, "..."},
+            {2, "|.|"},
+            {3, "..|"},
+            {4, "._|"},
+            {5, "|_|"},
+            {6, "|_."},
+            {7, "..|"}
+        };
+        
+        private readonly IDictionary<char, Digit> digits = new Dictionary<char, Digit>
+        {
+            {'0', new Digit(Bits[0], Bits[2], Bits[5])},
+            {'1', new Digit(Bits[1], Bits[3], Bits[3])},
+            {'2', new Digit(Bits[0], Bits[4], Bits[6])},
+            {'3', new Digit(Bits[0], Bits[4], Bits[4])},
+            {'4', new Digit(Bits[1], Bits[5], Bits[3])},
+            {'5', new Digit(Bits[0], Bits[6], Bits[4])},
+            {'6', new Digit(Bits[0], Bits[6], Bits[5])},
+            {'7', new Digit(Bits[0], Bits[7], Bits[7])},
+            {'8', new Digit(Bits[0], Bits[5], Bits[5])},
+            {'9', new Digit(Bits[0], Bits[5], Bits[7])}
         };
 
         public string ToLCD(string number)
@@ -26,7 +46,7 @@ namespace Console
             return stringBuilder.ToString();
         }
 
-        private char[] Pars(string number)
+        private IEnumerable<char> Pars(string number)
         {
             return number.ToCharArray();
         }
