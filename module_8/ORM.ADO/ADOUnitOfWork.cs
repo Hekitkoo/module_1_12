5b3013bed624595fe8d5c581b9917837bf373fb3
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-05-11 22:41:30 +0300

    Added ado.net implementation

---
+++ module_8/ORM.ADO/ADOUnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using ORM.ADO.Repositories;
+using ORM.Core.Interfaces;
+using ORM.Core.Models;
+
+namespace ORM.ADO
+{
+    public class ADOUnitOfWork : IUnitOfWork, IDisposable
+    {
+        public IRepository<Warehouse, int> Warehouses { get; private set; }
+        public IRepository<Route, int> Routes { get; private set; }
+        private SqlConnection Context { get; }
+        private SqlTransaction Transaction { get; }
+        
+        public ADOUnitOfWork(string connectionString)
+        {
+            Context = new SqlConnection(connectionString);
+            Context.Open();
+            Transaction = Context.BeginTransaction();
+            Warehouses = new WarehouseRepository(Transaction, Context);
+            Routes = new RouteRepository(Transaction, Context);
+        }
+
+        public void RollBack()
+        {
+            Transaction.Rollback();
+        }
+
+        public void Commit()
+        {
+           Transaction.Commit();
+        }
+
+        public void Dispose()
+        {
+            Transaction?.Dispose();
+
+            if (Context == null)
+            {
+                return;
+            }
+            Context.Close();
+            Context.Dispose();
+
+            Warehouses = null;
+            Routes = null;
+        }
+    }
+}
---
+++ module_8/ORM.ADO/Repositories/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace ORM.ADO.Repositories
+{
+    public abstract class Repository
+    {
+        private readonly SqlConnection context;
+
+        private readonly SqlTransaction transaction;
+
+        protected Repository(SqlTransaction transaction, SqlConnection context)
+        {
+            this.transaction = transaction;
+            this.context = context;
+        }
+
+        protected SqlCommand CreateCommand(string query)
+        {
+            return new(query, context, transaction);
+        }
+    }
+}
