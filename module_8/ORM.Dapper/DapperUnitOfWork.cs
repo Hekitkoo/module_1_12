bc67c046b91f28118dd27eba344eb8a34e924f14
Mykyta Sokurenko <Mykyta_Sokurenko@epam.com>
2021-05-26 00:03:59 +0300

    Added dapper

---
+++ module_8/ORM.Dapper/DapperUnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using ORM.Core.Interfaces;
+using ORM.Core.Models;
+using ORM.Dapper.Repositories;
+
+namespace ORM.Dapper
+{
+    public class DapperUnitOfWork : IUnitOfWork, IDisposable
+    {
+        public IRepository<Warehouse, int> Warehouses { get; private set; }
+        public IRepository<Route, int> Routes { get; private set; }
+
+        private SqlConnection Context { get; }
+
+        public DapperUnitOfWork(string connectionString)
+        {
+            Context = new SqlConnection(connectionString);
+            Context.Open();
+            Routes = new DapperRouteRepository(Context);
+        }
+
+        public void RollBack()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.transactions.transactionscope?redirectedfrom=MSDN&view=net-5.0
+        }
+
+        public void Commit()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.transactions.transactionscope?redirectedfrom=MSDN&view=net-5.0
+        }
+
+        public void Dispose()
+        {
+            if (Context == null)
+            {
+                return;
+            }
+            Context.Close();
+            Context.Dispose();
+        }
+    }
+}
---
+++ module_8/ORM.Dapper/Repositories/DapperRouteRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using ORM.Core.Interfaces;
+using ORM.Core.Models;
+using Dapper;
+
+namespace ORM.Dapper.Repositories
+{
+    public class DapperRouteRepository : IRepository<Route, int>
+    {
+        private SqlConnection connection;
+
+        public DapperRouteRepository(SqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Create(Route entity)
+        {
+            using IDbConnection c = connection;
+            const string createCommand = @"INSERT INTO [dbo].[Route](OriginWarehouseId, DestinationWarehouseId, Distance) 
+            OUTPUT INSERTED.RouteId values (@originWarehouseId, @destinationWarehouseId, @distance)";
+            c.Execute(createCommand, entity);
+        }
+
+        public Route GetById(int entityId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Route> GetAll()
+        {
+            List<Route> results;
+
+            using IDbConnection db = connection;
+            const string selectCommand = "SELECT * FROM  [dbo].[Route] WITH(NOLOCK)";
+            results = db.Query<Route>(selectCommand).ToList();
+
+            return results;
+        }
+
+        public void Update(Route entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int entityId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
